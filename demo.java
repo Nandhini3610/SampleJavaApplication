e3bfaf6b0aaad479624db8c01245067c52007415
NandhiniGoura <nandinigoura29@gmail.com>
2025-07-31 14:22:25 +0530

    second commit

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 import java.util.*;
-class Main{
-  public static void main(String[] args{
+class demo{
+  public static void main(String[] args){
      System.out.println("Hello World!");
     }
 }
