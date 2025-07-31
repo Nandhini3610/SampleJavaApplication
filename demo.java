50f73a14dc8f065b6fe975b4deb7a4c945d9870b
Nandhini3610 <nandinigoura29@gmail.com>
2025-07-31 14:52:16 +0530

    fourth commit

--- demo.java
+++ demo.java
@@ -3,5 +3,6 @@ class demo{
   public static void main(String[] args){
      System.out.println("Hello World!");
      System.out.println("Hii Nandhini");
+    System.out.println("SCM polling ");
     }
 }
